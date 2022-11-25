e6048f3ea7456ad387aff388effbcc5013fdb9fc
pya-h <p4ya.py@gmail.com>
2022-11-25 04:39:17 +0330

    styles regrouped in classes; shadow animation added to title section

--- views/dashboard.py
+++ views/dashboard.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import pandas as pd
 
 # preprocess data
-data = pd.read_csv('../models/avocado.csv')
+data = pd.read_csv('models/avocado.csv')
 data = data.query('type == "conventional" and region == "Albany"' )
 print(data)
 data["Date"] = pd.to_datetime(data['Date'], format="%Y-%m-%d")
@@ -14,48 +14,42 @@ data.sort_values("Date", inplace=True)
 dashboard = dash.Dash(__name__)
 
 # design the html layout
-dashboard.layout = html.Div( style={
-        "fontSize": "20px",
-        "marginTop": "10px"
-    },
-    children=[
-        html.Div(
-            style={
-                'margin':'2rem',
-                "textAlign": "center"
-            },
-            children=[
-                html.H1(children="Words Statistics"),
-                html.P(children="Here we count all the hashtags that are used among the comments of post viewers:"),
-            ]
-        ),
-        html.Hr(style={"border": "2px dashed lightgrey", "margin": "100px"}),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "bar",
-                    },
-                ],
-                "layout": {"title": "Number of Hashtags"},
-            },
-        ),
-        # dcc.Graph(
-        #     figure={
-        #         "data": [
-        #             {
-        #                 "x": data["Date"],
-        #                 "y": data["Total Volume"],
-        #                 "type": "lines",
-        #             }
-        #         ],
-        #         "layout": {"title": "Number of whatever"}
-        #     },
-        # )
-    ]
-)
+dashboard.layout = html.Div( className="container",
+                            children=[
+                                html.Div(
+                                    className="title",
+                                    children=[
+                                        html.H1(children="Words Statistics"),
+                                        html.P(children="Here we count all the hashtags that are used among the comments of post viewers:"),
+                                    ]
+                                ),
+                                html.Hr(className="separator"),
+                                dcc.Graph(
+                                    figure={
+                                        "data": [
+                                            {
+                                                "x": data["Date"],
+                                                "y": data["AveragePrice"],
+                                                "type": "bar",
+                                            },
+                                        ],
+                                        "layout": {"title": "Number of Hashtags"},
+                                    },
+                                ),
+                                    # dcc.Graph(
+                                    #     figure={
+                                    #         "data": [
+                                    #             {
+                                    #                 "x": data["Date"],
+                                    #                 "y": data["Total Volume"],
+                                    #                 "type": "lines",
+                                    #             }
+                                    #         ],
+                                    #         "layout": {"title": "Number of whatever"}
+                                    #     },
+                                    # )
+                                ]
+                    )
 
 # now run the server
 if __name__ == "__main__":
